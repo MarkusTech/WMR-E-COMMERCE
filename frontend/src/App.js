b4fe528ef9ad12663683f2052c0285741d38d469
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-19 15:03:10 +0800

    fontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./Routes.js";
+import SignupPage from "./pages/SignupPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,3 +1,4 @@
 import LoginPage from "../src/pages/Login.jsx";
+import SignupPage from "../src/pages/SignupPage.jsx";
 
-export { LoginPage };
+export { LoginPage, SignupPage };
