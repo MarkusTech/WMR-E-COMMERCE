7e87c43d12dcb886f57f567f7aac836bb2cae875
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-19 12:00:24 +0800

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -8,6 +8,13 @@ const Login = () => {
           Login to your account
         </h2>
       </div>
+      <div className="mt-8 sm:sx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6">
+            <div></div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
