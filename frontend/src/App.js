d0ae1592e50e191a0b26fd4780c0e0278b42b978
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-20 09:09:46 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
+        <Route />
       </Routes>
     </BrowserRouter>
   );
