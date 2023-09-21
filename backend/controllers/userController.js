baac4d0dc5be5b91b6b1f832e27a029775e67b39
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-21 13:33:33 +0800

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,18 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/user.js";
+import ErrorHandler from "../utils/ErrorHandler";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("Register User");
+  const { name, email, password } = req.body;
+  try {
+    const userEmail = await User.findOne({ email });
+
+    if (userEmail) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+  } catch (error) {
+    throw new Error();
+  }
 });
 
 export { registerUser };
